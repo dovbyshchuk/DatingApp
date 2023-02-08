42c9453589f77ddcb3b2344e6ddefa3b8f87a088
Volodymyr Dovbyshchuk <vova2570604@gmail.com>
2023-02-08 21:30:49 +0200

    Authentication basics

---
+++ API/Extentions/ApplicationServiceExtensions.cs
@@ -0,0 +1,22 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extentions
+{
+	public static class ApplicationServiceExtensions
+	{
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+		{
+			services.AddDbContext<DataContext>(options =>
+			{
+				options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+			});
+			services.AddCors();
+			services.AddScoped<ITokenService, TokenService>();
+
+			return services;
+		}
+	}
+}
